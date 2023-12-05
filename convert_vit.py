39cac7e55ad2ba974c38db6849d39c2682da290c
Wayne Feng <wei.feng@intel.com>
2023-12-05 21:12:16 +0800

    convert ViT to OpenVINO

--- convert_vit.py
+++ convert_vit.py
@@ -1,28 +1,24 @@
 import torch
-from pytorch_pretrained_vit import ViT
-
-from pathlib import Path
 import openvino as ov
-
+from pytorch_pretrained_vit import ViT
 import sys
 
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        model_id = str(sys.argv[1]).strip()
-    else:
+    if len(sys.argv) < 2:
         exit(-1)
-    as_fp16 = True if len(sys.argv) < 2 else int(sys.argv[2]) == 0
-    output_path = Path('/tmp/') if len(sys.argv) < 3 else Path(sys.argv[3].strip())
+    model_id = sys.argv[1].strip()
+    #save_as_fp16 = bool(int(sys.argv[2]) == 0)
 
+    # Load model
     model = ViT(model_id, pretrained=True)
     model.eval()
 
-    img_sz = 384 if model_id.endswith('imagenet1k') else 224
-    img = torch.randn([1, 3, img_sz, img_sz])
-    ov_model = ov.convert_model(model, example_input=img, input=[1,3,img_sz,img_sz])
-    if output_path.exists() and output_path.is_dir():
-        output_path += f"vit_{model_id}_fp{16 if as_fp16 else 32}.xml"
-        ov.save_model(ov_model, str(output_path), compress_to_fp16=as_fp16)
-    else:
-        ov.save_model(ov_model, f"/tmp/vit_{model_id}_fp{16 if as_fp16 else 32}.xml")
+    # Convert and save OV model
+    img_size = 384 if model_id.endswith("imagenet1k") else 224
+    img = torch.randn([1,3,img_size,img_size])
+    ov_model = ov.convert_model(model, example_input=img)
+    #ov_path = f"/tmp/vit_{model_id.lower()}_fp{16 if save_as_fp16 else 32}.xml"
+    ov_path = f"/tmp/vit_{model_id.lower()}_fp16.xml"
+    ov.save_model(ov_model, ov_path, compress_to_fp16=True)
+    ov_path = f"/tmp/vit_{model_id.lower()}_fp32.xml"
+    ov.save_model(ov_model, ov_path, compress_to_fp16=False)
